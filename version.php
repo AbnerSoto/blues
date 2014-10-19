1562007115f00fb35ffed1a0ec95686743d423b2
Abner Soto <aosoto@gmail.com>
2014-10-19 14:25:29 -0700

    initial commit

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+define ('TODAY', 'March 16, 2014');
+
+echo '<p>Today is ' . TODAY . '.<br /> This server is running version <b>' . PHP_VERSION . '</b> of PHP on the <b>' . PHP_OS .'</b> operating system.</p>';
+
+
+?>
